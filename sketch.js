ecfa38aca2f68bf55df51f3a5f46f80f160cfb8d
Leonidas Lundell <leonidas.lundell@gmail.com>
2023-03-11 21:48:28 +0100

    Modified random walk and changed appearance

--- sketch.js
+++ sketch.js
@@ -1,13 +1,13 @@
 let mic, fft;
 
-let min_point = 2
-let max_point = 150
+let min_point = 10
+let max_point = 300
 
 function setup() {
   
   //displayWidth takes device size
   createCanvas(displayWidth, displayHeight);
-  frameRate(10);
+  frameRate(30);
   
   // setting up the audio;
   mic = new p5.AudioIn();
@@ -18,7 +18,7 @@ function setup() {
 }
 
 function draw() {
-  background(0, 100);
+  background(1000, 50);
   spectrum = fft.analyze()
   
 
@@ -33,8 +33,8 @@ function draw() {
   treble = map(fft.getEnergy(700, 20000), 0, 255, 
              min_point, max_point)
   
-  fuzz_draw(bass, bass, '#4B0082', 200)
-  fuzz_draw(lowMid, lowMid, '#0000FF', 200)
+  fuzz_draw(bass, bass, '#4B0082')
+  fuzz_draw(lowMid, lowMid, '#0000FF')
   fuzz_draw(mid, mid, '#00FF00', )
   fuzz_draw(highMid, highMid, '#FFFF00')
   fuzz_draw(treble, treble, '#FF0000')
@@ -70,7 +70,7 @@ function fuzz_draw(x, y, col, step = 1){
   circle_y = random(displayHeight)
   
   
-  for(jiggle = 0.25; jiggle < 100; jiggle += .25){
+  for(jiggle = 1; jiggle < 500; jiggle += 1){
     
     fill(color(col))
     noStroke()
@@ -80,8 +80,8 @@ function fuzz_draw(x, y, col, step = 1){
     jiggle_x = random(-step, step)
     jiggle_y = random(-step, step)
     
-    circle_x += jiggle_x
-    circle_y += jiggle_y
+    circle_x += jiggle_x * map(x, 0, displayWidth, 1, 20)
+    circle_y += jiggle_y * map(y, 0, displayHeight, 1, 20)
   }
 }
 
